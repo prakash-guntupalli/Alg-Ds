24e1c7b10540c60655b4e60081cb853932568dfa
prakash-guntupalli <email2prakashg@gmail.com>
2020-05-26 23:25:17 -0400

    selection sort

---
+++ alg/sort/selectionSort.js
@@ -0,0 +1,26 @@
+// consider 1st value as min and iterate through out the array if you find any value less than min then
+// replace that index with min and swap those values.
+
+function selectionSort(arr){
+
+    for(var i = 0; i < arr.length; i++){
+        var min = i;
+
+        for(var j = i+1; j < arr.length; j++){
+            if (arr[j] < arr[min]){
+                min = j;
+            }
+        }
+
+        if(i !== min){
+            var temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
+    return arr;
+}
+
+
+//selectionSort([ 0, 2, 12, 8, 6, 16, 24]) => [0,2,6,8,12,16,24]
