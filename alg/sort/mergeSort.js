8a2b3c4991940aa25b19cc3cb65d30e75145b4c3
prakash-guntupalli <email2prakashg@gmail.com>
2020-05-30 23:00:11 -0400

    merge sort updates

--- alg/sort/mergeSort.js
+++ alg/sort/mergeSort.js
@@ -1,11 +1,10 @@
-
+// merge the small arrays while sorting the elements
 function mergeArrays(arr1, arr2) {
     let result = [];
     let x = 0;
     let y = 0;
 
     while(x < arr1.length && y < arr2.length){
-        
         if(arr1[x] < arr2[y]){
             result.push(arr1[x]);
             x++;
@@ -13,7 +12,6 @@ function mergeArrays(arr1, arr2) {
             result.push(arr2[y]);
             y++;
         }
-
     }
 
     while(x < arr1.length){
@@ -30,17 +28,16 @@ function mergeArrays(arr1, arr2) {
 }
 
 
+// split the given array into single elements
 function mergeSort(arr){
 
     if (arr.length <= 1) return arr;
 
     var mid = Math.floor(arr.length/2);
-    var left = [arr[0], arr[mid]];
-    var right = [arr[mid+1], arr[arr.length-1]];
+    var left = mergeSort(arr.slice(0,mid));
+    var right = mergeSort(arr.slice(mid));
 
-    console.log("left", left);
-    console.log("right", right);
-
+    return mergeArrays(left, right);
 }
 
 
