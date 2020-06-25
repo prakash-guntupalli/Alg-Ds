49899bb9e8b82f924379f3027a73197348fd97fe
prakash-guntupalli <email2prakashg@gmail.com>
2020-06-24 21:53:35 -0500

    Doubly linked list unshift

--- ds/doublyLinkedList.js
+++ ds/doublyLinkedList.js
@@ -26,6 +26,7 @@ class DoublyLinkedList {
         } else {
             this.tail.next = latest;
             latest.prev = this.tail;
+            this.tail = latest;
         }
 
         this.length++;
@@ -48,18 +49,30 @@ class DoublyLinkedList {
     }
 
     shift(){
-        if(this.length === 0) return undefined;
-        let oldHead = this.head;
-        if(this.length ===1){
-            this.head = null;
-            this.tail = null;
-        } else {
-            this.head = oldHead.next;
-            this.head.prev = null;
-            oldHead.next = null;
-        }
+        if(!this.head) return undefined;
+        var newHead = this.head.next;
+        var oldHead = this.head;
+        newHead.prev = null;
+        oldHead.next = null;
+        this.head = newHead;
+
         this.length--;
         return oldHead;
+    }
+
+    unshift(val){
+        let newNode = new Node(val);
+
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
     }
 }
 
