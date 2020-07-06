18d9ac1fcb0e917e66e7e417ff281d29f88c630f
prakash-guntupalli <email2prakashg@gmail.com>
2020-07-05 23:48:41 -0500

    binary search tree part2

--- ds/trees/binarySearchTree.js
+++ ds/trees/binarySearchTree.js
@@ -1,6 +1,6 @@
 class Node {
-    constructor(val){
-        this.value = val;
+    constructor(value){
+        this.value = value;
         this.left = null;
         this.right = null;
     }
@@ -10,23 +10,33 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
-    insert(val){
-        let newNode = new Node(val);
+    insert(value){
+        let newNode = new Node(value);
         if(this.root === null){
             this.root = newNode;
             return this;
         } else {
             let current = this.root;
             while(true){
-                if(val < current.value){
+                if(value === current.value) return false;
+                if(value < current.value){
                     if(current.left === null){
                         current.left = newNode;
                         return this;
                     } else {
                         current = current.left;
                     }
+                } else if(value > current.value) {
+                    if(current.right === null){
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
                 }
             }
         }
     }
 }
+
+let tree = new BinarySearchTree();
