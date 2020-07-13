f7351a2220a18b4acbda44b2c30bcd1bd1036c01
prakash-guntupalli <email2prakashg@gmail.com>
2020-07-12 22:53:42 -0500

    recursion ex4

--- recursion/second.js
+++ recursion/second.js
@@ -24,3 +24,14 @@ function recursiveRange(num){
  }
  
  recursiveRange(4); //10
+
+
+
+ // fibonacci series
+
+ function fib(num){
+    if(num <= 2) return 1;
+    return fib(num - 1) + fib(num - 2);
+ }
+
+ fib(10);       //55    (1,1,2,3,5,8,13,21,34,55)
