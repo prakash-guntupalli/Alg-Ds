36dbab36a0d9513580652537590107db05d38beb
prakash-guntupalli <email2prakashg@gmail.com>
2020-07-27 23:07:02 -0500

    binary heap intro

---
+++ ds/trees/binaryHeap.js
@@ -0,0 +1,8 @@
+// Binary Heap is similar to binary search tree, each parent can have 2 children and left children are filled first
+// Binary heaps are used to implement priority Queues and in graph traversal algorithms.
+
+// MaxBinary Heap - Parent nodes are always larger than child nodes (no order between siblings)
+// MinBinary Heap - Parent nodes are smaller than child nodes
+
+// For any index of an array 'n', left child will be at '2n+1' & right child at '2n+2'
+
