7ca8e380df66fff95f349acbccdec1beb18a7d11
prakash-guntupalli <email2prakashg@gmail.com>
2020-07-29 23:57:10 -0500

    binary heap remove

--- ds/trees/binaryHeap.js
+++ ds/trees/binaryHeap.js
@@ -32,6 +32,49 @@ class MaxBinaryHeap {
 
         }
     }
+
+    // remove the root element and make recent added one as root, now bubble down comparing children values
+    remove() {
+        const max = this.values[0];
+        const end = this.values.pop();
+        if(this.values.length > 0){
+            this.values[0] = end;
+            this.bubbleDown();
+        }
+        return max;
+    }
+    bubbleDown(){
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while(true){
+            let rightChildIdx = 2*idx +2 ;
+            let leftChildIdx = 2*idx +1;
+            let rightChild, leftChild;
+            let swap = null;
+
+            if(leftChildIdx < length){
+                leftChild = this.values[leftChildIdx];
+                if(leftChild > element){
+                    swap = leftChildIdx;
+                }
+            }
+            if(rightChildIdx < length){
+                rightChild = this.values[rightChildIdx];
+                if(
+                    (swap === null && rightChild > element) ||
+                    (swap !== null && rightChild > leftChild)
+                ){
+                    swap = rightChildIdx;
+                }
+            }
+            if(swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+        }
+
+    }
 }
 
 let heap = new MaxBinaryHeap();
