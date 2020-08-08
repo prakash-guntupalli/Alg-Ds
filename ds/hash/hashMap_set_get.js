23cce2971452637af3f884edcb851395aeaf2284
prakash-guntupalli <email2prakashg@gmail.com>
2020-08-07 23:14:05 -0500

    hash table keys

--- ds/hash/hashMap_set_get.js
+++ ds/hash/hashMap_set_get.js
@@ -37,13 +37,27 @@ class HashTable {
       }
       return undefined;
     }
-
+    keys(){
+      let keysArr = [];
+      for(let i=0; i<this.keyMap.length; i++){
+        if(this.keyMap[i]){
+          for(let j=0; j<this.keyMap[i].length; j++){
+            if(!keysArr.includes(this.keyMap[i][j][0])){
+              keysArr.push(this.keyMap[i][j][0])
+            }
+          }
+        }
+      }
+      return keysArr;
+    }
     values(){
       let valuesArr = [];
       for(let i=0; i<this.keyMap.length; i++){
         if(this.keyMap[i]){
           for(let j=0; j<this.keyMap[i].length; j++){
-            valuesArr.push(this.keyMap[i][j][1])
+            if(!valuesArr.push(this.keyMap[i][j][1])){
+              valuesArr.push(this.keyMap[i][j][1])
+            }
           }
         }
       }
