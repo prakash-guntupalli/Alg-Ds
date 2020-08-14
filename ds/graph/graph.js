8bb09ec0655b8ea167da739ac7edebdb643e82fd
prakash-guntupalli <email2prakashg@gmail.com>
2020-08-13 22:48:29 -0500

    remove connections

--- ds/graph/graph.js
+++ ds/graph/graph.js
@@ -26,6 +26,15 @@ class Graph {
         this.adjacencyList[vertex1].push(vertex2);
         this.adjacencyList[vertex2].push(vertex1);
     }
+    // remove connections
+    removeEdge(vertex1, vertex2){
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter( v =>
+            v !== vertex2
+        );
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter( v =>
+            v !== vertex1
+        );
+    }
 } 
 
 let g = new Graph();
